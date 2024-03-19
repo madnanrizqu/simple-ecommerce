db878ecc4005fbaa4f6ac2edb5e7cde486932596
rizquadnan <madnanrizqullah@gmail.com>
2024-03-19 08:45:29 +0700

    init component lib

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import '@radix-ui/themes/styles.css';
+
+import "@radix-ui/themes/styles.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme appearance="light" accentColor="blue" radius="none">
+          {children}
+          {process.env.NEXT_PUBLIC_SHOW_THEME_PANEL && <ThemePanel />}
+        </Theme>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@radix-ui/themes";
+
 export default function Home() {
   return (
+    <>
     <h1>Hello Base</h1>
+    <Button>From Radix Themes</Button>
+    </>
   );
 }
