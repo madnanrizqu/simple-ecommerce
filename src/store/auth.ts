4762339f0306bc904b97916247ee7882d77745af
rizquadnan <madnanrizqullah@gmail.com>
2024-03-24 08:39:34 +0700

    implement storage persist

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,5 +1,6 @@
 import { LoggedInUser } from "@/type/user";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface State {
   user: LoggedInUser | null;
@@ -8,9 +9,16 @@ interface State {
   setUser: (user: LoggedInUser) => void;
 }
 
-export const useAuthStore = create<State>()((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-  token: null,
-  setToken: (token) => set({ token }),
-}));
+export const useAuthStore = create<State>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      token: null,
+      setToken: (token) => set({ token }),
+    }),
+    {
+      name: "auth_storage",
+    }
+  )
+);
