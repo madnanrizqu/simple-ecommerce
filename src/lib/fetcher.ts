4998ac87ad16e3c29234f77aaeab1515f02bba7e
rizquadnan <madnanrizqullah@gmail.com>
2024-03-25 06:39:24 +0700

    add 401 toast

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,6 +1,29 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
-export const fetcher = axios.create({
+const fetcher = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_FE_BASE_URL ?? "http://localhost:3000/api/v1",
 });
+
+fetcher.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const status = err?.response?.status || null;
+
+    console.log(status);
+
+    // if unauthorized redirect to login page
+    if (status === 401) {
+      toast("Sesi anda habis, silahkan login ulang", {
+        autoClose: 1500,
+        onClose: () => {
+          window.location.href = "/login";
+        },
+      });
+    }
+    return Promise.reject(err);
+  }
+);
+
+export { fetcher };
